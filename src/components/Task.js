840409c214e1016a4a9b3f417daf48b9886be260
Mahadi Hassan <mahadi.hassan@northsouth.edu>
2020-09-27 11:14:14 +0600

    Static todo list rendered using context api

---
+++ src/components/Task.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+
+const Task = ({ task }) => {
+    return (
+        <li className="list-item">
+            <span>{task.title} </span>
+            <div>
+                <button
+                    className="btn-delete task-btn"
+
+                >
+                    <i className="fas fa-trash-alt"></i>
+                </button>{' '}
+                <button className="btn-edit task-btn" >
+                    <i className="fas fa-pen"></i>
+                </button>
+            </div>
+        </li>
+    )
+}
+
+export default Task
